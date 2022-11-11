e17b3241306d3137967167658594fef0a4e01d8f
sebastianb <juansebastianbb05@outlook.com>
2022-11-11 16:39:17 -0500

    feat: add test 2e2 for delete page on cypress

---
+++ cypress/cypress/e2e/delete-page.spec.cy.js
@@ -0,0 +1,37 @@
+describe('Testing basic Angular registration', () => {
+    before(() => {
+        cy.visit('http://localhost:2368/ghost/#/signin')
+    })
+    it('Create an user and login', () => {
+        cy.get('form').within(() => {
+            cy.get('input[name="identification"]').type('a@a.com')
+            cy.get('input[name="password"]').type('GhAuthorEx1*')
+            cy.get('button.login.gh-btn.gh-btn-blue.gh-btn-block.gh-btn-icon.ember-view').click()
+        });
+        cy.wait(2000);
+        //Redirected to login
+        cy.get('a[href="#/pages/"]').should('be.visible');
+        cy.wait(1000);
+        cy.get('a[href="#/pages/"]').click();
+        cy.wait(2000);
+        let pageBeforeQuantity = 0;
+        cy.get('h3[class="gh-content-entry-title"]')
+            .then(($value) => {
+                pageBeforeQuantity = $value.length
+            });
+        cy.wait(2000);
+        cy.get('a.ember-view.permalink.gh-list-data.gh-post-list-title').first().click();
+        cy.wait(2000);
+        cy.get('button.post-settings').click();
+        cy.wait(2000);
+        cy.get('button.settings-menu-delete-button').click();
+        cy.wait(2000);
+        cy.get('button.gh-btn.gh-btn-red.gh-btn-icon.ember-view').click();
+        cy.wait(2000);
+        cy.wait(2000);
+        cy.get('h3[class="gh-content-entry-title"]')
+            .then(($value) => {
+                expect($value.length).to.equal(pageBeforeQuantity -1)
+            });
+    });
+})

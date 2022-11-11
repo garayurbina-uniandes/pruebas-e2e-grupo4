1488b7e1247c80548179b55728b375194861505b
sebastianb <juansebastianbb05@outlook.com>
2022-11-11 10:07:50 -0500

    feat: change credentials on cypress

--- cypress/cypress/e2e/create-page.spec.cy.js
+++ cypress/cypress/e2e/create-page.spec.cy.js
@@ -4,8 +4,8 @@ describe('Testing basic Angular registration', () => {
     })
     it('Create an user and login', () => {
         cy.get('form').within(() => {
-            cy.get('input[name="identification"]').type('j.becerrab@uniandes.edu.co')
-            cy.get('input[name="password"]').type('alemania12')
+            cy.get('input[name="identification"]').type('a@a.com')
+            cy.get('input[name="password"]').type('GhAuthorEx1*')
             cy.get('button.login.gh-btn.gh-btn-blue.gh-btn-block.gh-btn-icon.ember-view').click()
         });
         cy.wait(2000);

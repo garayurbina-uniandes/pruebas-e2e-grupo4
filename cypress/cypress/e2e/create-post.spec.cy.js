9d935cfd4387f69ca8af05a0ab2c5b3b839b9f45
Alexander Mora Ruiz <a.morar2@uniandes.edu.co>
2022-11-13 15:56:24 -0500

    Agregar escenario de programar la publicacion de una entreda

--- cypress/cypress/e2e/create-post.spec.cy.js
+++ cypress/cypress/e2e/create-post.spec.cy.js
@@ -42,6 +42,51 @@ describe('Como usuario creo una entrada y la publico inmediatamente, luego verif
     })
 });
 
+describe('Como usuario creo una entrada, la programo para publicación en 5 minutos, espero ese tiempo y luego verifico que esté listada', () => {
+    before(() => {
+        cy.visit('http://localhost:2368/ghost/#/signin')
+    })
+    it('Login', () => {
+        cy.get('form').within(() => {
+            cy.get('input[name="identification"]').type('a@a.com')
+            cy.get('input[name="password"]').type('GhAuthorEx1*')
+            cy.get('button.login.gh-btn.gh-btn-blue.gh-btn-block.gh-btn-icon.ember-view').click()
+        });
+    })
+
+    it('Create a post', () => {
+        cy.wait(2000);
+        cy.get('a[href="#/posts/"]').should('be.visible');
+        cy.wait(1000);
+        cy.get('a[href="#/posts/"]').first().click();
+        cy.wait(1000);
+        let postsBeforeCreate = 0;
+        cy.get('h3[class="gh-content-entry-title"]')
+            .then(($value) => {
+                postsBeforeCreate = $value.length
+            });
+        cy.get('a.ember-view.gh-btn.gh-btn-green').click();
+        cy.wait(2000);
+        cy.get('textarea.gh-editor-title.ember-text-area.gh-input.ember-view').type('Kraken Test 5')
+        cy.wait(2000);
+        cy.get('div[data-placeholder="Begin writing your post..."]').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+        cy.wait(2000);
+        cy.get('.gh-publishmenu').click();
+        cy.wait(2000);
+        cy.get('div.gh-publishmenu-radio-content:last').click();
+        cy.wait(2000);
+        cy.get('button.gh-publishmenu-button').click();
+        cy.wait(2000);
+        cy.get('article.gh-notification.gh-notification-passive.ember-view').should('be.visible');
+        cy.wait(2000);
+        cy.get('a.blue.link.fw4.flex.items-center.ember-view').click();
+        cy.wait(2000);
+        cy.get('h3[class="gh-content-entry-title"]')
+            .then(($value) => {
+                expect($value.length).to.equal(postsBeforeCreate + 1)
+            });
+    })
+});
 
 // describe('Testing basic Angular registration', () => {
 //     before(() => {

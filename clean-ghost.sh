85faebea1177e7d0074e0bbc6dbfcf43b78954c6
Alexander Mora Ruiz <a.morar2@uniandes.edu.co>
2022-11-13 15:25:25 -0500

    Actualizar scripts de configuración

---
+++ clean-ghost.sh
@@ -0,0 +1,3 @@
+cd ghost
+rm -drf content current versions
+rm -f .ghost-cli .ghostpid config.development.json
--- setup-ghost.sh
+++ setup-ghost.sh
@@ -4,8 +4,15 @@ echo "Current version:" $NODE_VERSION
 
 if [[ $NODE_VERSION == v14* ]] # Checking node version
 then
-    echo "Match"
+    echo "You are using NodeJS: " $NODE_VERSION
 else
     echo "Please use a NodeJS v14.x"
-    exit(1)
+    exit 1
 fi
+
+echo "Installing Ghost v3.41.2"
+cd ghost
+ghost install --force 3.41.1 --local
+
+echo "Installing SqLite"
+npm install sqlite3 --save

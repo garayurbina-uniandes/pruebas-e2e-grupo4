deae4ef2255111813fd5fbfcdf778d3ac14794f8
Alexander Mora Ruiz <a.morar2@uniandes.edu.co>
2022-11-13 15:39:02 -0500

    Actualizar script para eliminar ghost

--- clean-ghost.sh
+++ clean-ghost.sh
@@ -1,6 +1,3 @@
 cd ghost
-# ghost stop
 ghost uninstall --force
-# rm -drfv content current versions node_modules
-# rm -fv .ghost-cli .ghostpid config.development.json package-lock.json package.json
 rm -fv package-lock.json package.json

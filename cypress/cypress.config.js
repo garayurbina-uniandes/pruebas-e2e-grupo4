4d8e1a4ba762543f0a8d509bc9bfec1601dc9708
Alexander Mora Ruiz <a.morar2@uniandes.edu.co>
2022-11-13 19:51:37 -0500

    Configurar orden de los tests

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -3,5 +3,18 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: [
+      "cypress/e2e/register.spec.cy.js",
+      "cypress/e2e/login.spec.cy.js",
+      "cypress/e2e/create-tag.spec.cy.js",
+      "cypress/e2e/view-tag-spec.cy.js",
+      "cypress/e2e/delete-tag-spec.cy.js",
+      "cypress/e2e/create-page.spec.cy.js",
+      "cypress/e2e/add-image-page.spec.cy.js",
+      "cypress/e2e/delete-page.spec.cy.js",
+      "cypress/e2e/create-post.spec.cy.js",
+      "cypress/e2e/update-post.spec.cy.js",
+      "cypress/e2e/delete-post.spec.cy.js",
+    ]
   },
 };
